aa90b4ea5a96db04b92536cb722b1842df47a9c5
Frank Horbelt <lifequesttherapy@gmail.com>
2025-05-07 21:58:06 -0500

    chore: remove node-fetch; use native fetch in Node 18+

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // index.js
 const express    = require('express');
 const bodyParser = require('body-parser');
-const fetch      = require('node-fetch');
 const path       = require('path');
 
 const app = express();
